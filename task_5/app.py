b115e7ddd8df76689d290b144e2e2a6846c56684
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-04-10 05:49:43 +0300

    add css classes

--- task_5/app.py
+++ task_5/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 from search_engine import VectorIndex, SearchEngine
+from document_mapper import DocumentMapper
+from config import DOCS_DIR
+
+mapper = DocumentMapper(DOCS_DIR)
 
 app = Flask(__name__)
 
@@ -15,7 +19,15 @@ def home():
 
     if request.method == "POST":
         query = request.form.get("query")
-        results = engine.search(query) # type: ignore
+        raw_results = engine.search(query) # type: ignore
+
+        results = [
+            {
+                "url": mapper.get_url(doc_id),
+                "score": score
+            }
+            for doc_id, score in raw_results
+        ]
 
     return render_template("index.html", results=results)
 
--- task_5/config.py
+++ task_5/config.py
@@ -1,1 +1,2 @@
 TFIDF_DIR = "../task_4/tfidf_tokens"
+DOCS_DIR = "../task_1/index.txt"
